4bdae1043f2b0a3e2fdb38db878f14943beda8ad
rrthakur <rt25669@gmail.com>
2020-09-25 11:08:05 +0530

    first commit

---
+++ README.m
@@ -0,0 +1,11 @@
+“#initialwebtechfinal>>README.m
+git init
+git add .
+git commit -m first commit
+git remote add origin https://github.com/rrthakur/initialwebtechfinal.git
+git push -u origin master
+
+
+
+
+echo “#initialwebtechfinal
